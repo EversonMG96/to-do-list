2c68f74978d81a78788e24728a7928fdce913921
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-12 12:09:25 -0300

    create function saveData

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -14,13 +14,20 @@ function addTask() {
        li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
-        e.target.classList.toogle("checked");
+        e.target.classList.toggle("checked");
+        saveData();
     }
     else if (e.target.tagName ==="SPAN") {
         e.target.parentElement.remove();
+        saveData();
     }
 },false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
