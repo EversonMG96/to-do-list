da4b8cabae5f0e6d40347e301bbb3b3b31f75f88
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-12 11:10:32 -0300

    add elements in the javascript function addTask

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -6,6 +6,9 @@ function addTask() {
         alert ("You must write something!");
     }
     else {
-       let li = document.createElement("li"); 
+       let li = document.createElement("li");
+       li.innerHTML = inputBox.value; 
+       listContainer.appendChild(li);
+       
     }
 }
