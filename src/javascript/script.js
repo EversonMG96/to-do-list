d4ce9d82fe17e8390c94050f09a1eabf357254a1
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-12 11:14:57 -0300

    add javascript line

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -9,6 +9,7 @@ function addTask() {
        let li = document.createElement("li");
        li.innerHTML = inputBox.value; 
        listContainer.appendChild(li);
-       
+
     }
+    inputBox.value = "";
 }
