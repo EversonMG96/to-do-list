a9b508609167fc5ece44f2bddb35bc5f98bc01f0
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-12 12:11:48 -0300

    create function showTask

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -31,3 +31,7 @@ listContainer.addEventListener("click", function(e) {
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
